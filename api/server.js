7ea0527d3fd1ff89a6141f09e4540b52ac5de8f5
Alvaro Fernandez Garcia <afernandezgarcia@serbatic.es>
2019-07-24 14:32:45 +0200

    first commit api

---
+++ api/server.js
@@ -0,0 +1,35 @@
+import 'dotenv';
+import express from 'express';
+import bodyParser from 'body-parser';
+import rssParser from 'rss-parser';
+import MEDIA_LIST from './utils/media';
+
+const server = express();
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+server.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+const parser = new rssParser();
+
+server.get('/news/:media', ({ params }, res) => {
+  const indexMedia = MEDIA_LIST.findIndex(item => item.name === params.media);
+  if (indexMedia > -1) {
+    parser.parseURL(MEDIA_LIST[indexMedia].url).then(feed => {
+      console.log(feed.title);
+      feed.items.forEach(item => {
+        console.log(item.title + ':' + item.link);
+      });
+    });
+  }
+  return { title: 'Hello World' };
+});
+
+server.listen(process.env.PORT || 3001, () => {
+  console.log(`Server is up and running at port ${process.env.PORT || 3001}`);
+});
---
+++ api/utils/media.js
@@ -0,0 +1,6 @@
+export default [
+  {
+    name: 'ABC_ESPANA',
+    url: 'https://www.abc.es/rss/feeds/abc_EspanaEspana.xml'
+  }
+];
